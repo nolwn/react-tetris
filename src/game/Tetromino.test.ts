8e64c76f4b2d0f7baf6e38e939698c7df5c9a0f4
nolwn <nolan.hellyer@gmail.com>
2021-02-11 21:52:51 -0800

    Rotate s tetromino.

--- src/game/Tetromino.test.ts
+++ src/game/Tetromino.test.ts
@@ -1,29 +1,43 @@
-import { OTetromino } from "./Tetromino";
+import { parseFile } from "./snapshots/parseSnapshot";
+import { OTetromino, STetromino } from "./Tetromino";
 
 describe("O Tetromino", () => {
 	test("Creates new O", () => {
 		const o = new OTetromino();
-		const expectedPattern = [
-			false,
-			false,
-			false,
-			false,
-			false,
-			true,
-			true,
-			false,
-			false,
-			true,
-			true,
-			false,
-			false,
-			false,
-			false,
-			false,
-		];
+		const expectedPattern = parseFile("./o.txt");
 
 		expect(o.box).toBe(4);
 		expect(o.pattern).toEqual(expectedPattern);
 		expect(o.color).toEqual("yellow");
+	});
+
+	test("Does not rotate", () => {
+		const o = new OTetromino();
+		const expectedPattern = parseFile("./o.txt");
+
+		o.clockwise();
+		expect(o.pattern).toEqual(expectedPattern);
+
+		o.counterClockwise();
+		expect(o.pattern).toEqual(expectedPattern);
+	});
+});
+
+describe("S Tetromino", () => {
+	test("Create a new S", () => {
+		const s = new STetromino();
+		const expectedPattern = parseFile("./s.txt");
+
+		expect(s.box).toBe(3);
+		expect(s.pattern).toEqual(expectedPattern);
+		expect(s.color).toBe("green");
+	});
+
+	test("Rotates clockwise", () => {
+		const s = new STetromino();
+		const expectedPattern = parseFile("./s-1x.txt");
+
+		s.clockwise();
+		expect(s.pattern).toEqual(expectedPattern);
 	});
 });
--- src/game/Tetromino.ts
+++ src/game/Tetromino.ts
@@ -19,20 +19,65 @@ const OPattern = [
 	false,
 ];
 
-export abstract class Tetromino {
-	abstract pattern: boolean[];
-	abstract box: number;
-	abstract color: CellColor;
-}
+const SPattern = [false, true, true, true, true, false, false, false, false];
 
-export class OTetromino implements Tetromino {
+export abstract class Tetromino {
 	pattern: boolean[];
 	box: number;
 	color: CellColor;
+	rotation: number;
 
+	constructor(pattern: boolean[], box: number, color: CellColor) {
+		this.pattern = pattern;
+		this.box = box;
+		this.color = color;
+		this.rotation = 0;
+	}
+
+	abstract clockwise(): void;
+
+	abstract counterClockwise(): void;
+
+	protected rotate(): Tetromino {
+		const rotated = [];
+
+		for (let i = 0; i < this.box; i++) {
+			for (let j = this.box - 1; j >= 0; j--) {
+				const idx = this.box * j + i;
+				rotated.push(this.pattern[idx]);
+			}
+		}
+
+		this.pattern = rotated;
+
+		return this;
+	}
+}
+
+export class OTetromino extends Tetromino {
 	constructor() {
-		this.pattern = OPattern;
-		this.box = 4;
-		this.color = "yellow";
+		super(OPattern, 4, "yellow");
+	}
+
+	clockwise(): void {
+		return;
+	}
+
+	counterClockwise(): void {
+		return;
+	}
+}
+
+export class STetromino extends Tetromino {
+	constructor() {
+		super(SPattern, 3, "green");
+	}
+
+	clockwise(): void {
+		this.rotate();
+	}
+
+	counterClockwise(): void {
+		this.rotate();
 	}
 }
